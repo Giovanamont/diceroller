67d6c03ffc40c3507fe5cf100e008d1eed75a51d
giovana <giovana280721@gmail.com>
2024-04-15 19:40:52 -0300

    trabalhando com classes

---
+++ DiceRoller/Dice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    class Dice
+    {
+        public Dice() {
+        
+        }
+
+        public Dice(int lados)
+        {
+            Lados = lados;
+        }
+
+        //Atributos
+        private int Lados { get; set; }
+
+        //Métodos
+        public int Rolar()
+        {
+            //Retorna o valor da rolagem do dado.
+            return new Random().Next(1, Lados + 1);
+        }
+    }
+}
---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Maui.Storage;
+
+namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            DiceTypePicker.SelectedIndex = 0;
+        }
+
+        private void DiceBtn_Clicked(object sender, EventArgs e)
+        {
+            //Instanciação de um novo objeto dice do tipo Dice, através do construtor Dice();
+            int sidesNumber = Convert.ToInt32(DiceTypePicker.SelectedItem);
+            Dice dice = new Dice(sidesNumber);
+            DiceResult.Text = Convert.ToString( dice.Rolar() );
+        }
+
+        private void DiceTypePicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DiceTypeImage.Source = $"d{DiceTypePicker.SelectedItem.ToString()}.png";
+            
+        }
+    }
+
+}
